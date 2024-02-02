f959a61da3f3c913bc70dd056c3eeed589b89c7c
Paul Johann Flade <paul-johann@flade-privat.de>
2024-02-02 11:59:56 +0100

    challange solved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Greeting name="Bertram" isCoach={false} />
+      <Greeting name="Waldemar" isCoach={false} />
+      <Greeting name="Feline" isCoach={true} />
+    </div>
+  );
+}
+
+function Greeting({ name, isCoach }) {
+  return (
+    <div>
+      <h1>`Hello, {isCoach ? "Coach" : name}!`</h1>
+    </div>
+  );
 }
